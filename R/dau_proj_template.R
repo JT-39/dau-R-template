c9c8fea559febcd973fdfac8c737a0237a757882
Jake Tufts <jake.tufts@education.gov.uk>
2024-03-29 12:47:44 +0000

    testthat working in proj setup, also further personalisation of general pkg

--- R/dau_proj_template.R
+++ R/dau_proj_template.R
@@ -1,13 +1,14 @@
 #' Create standard analysis template for RStudio project
 #'
 #' @param path path of the new project
-#' @param include_structure_for_pkg additional Analysis for publication folder
-#' @param include_github_gitignore should a gitingore file be created
+#' @param include_structure_for_pkg additional structure for package development
+#' @param include_github_gitignore should a strict gitingore file for GitHub be created
 #' @param ... additional parameters, currently not used
 #'
 #' @return no return values, just the folders and README are created
 #' @importFrom utils installed.packages
-#' @importFrom usethis create_package use_test
+#' @importFrom withr with_dir
+#' @importFrom usethis create_package create_project use_testthat use_test
 #' @export
 dau_proj_template <- function(
     path,
@@ -19,6 +20,9 @@ dau_proj_template <- function(
   if (include_structure_for_pkg) {
 
     usethis::create_package(path = path, open = FALSE)
+    withr::with_dir(path, {
+      usethis::use_testthat()
+    })
   #   
   #   # # Extras
   #   # usethis::use_r("helpers")
@@ -32,39 +36,43 @@ dau_proj_template <- function(
   #   
   } else {
     # ensure that the path exists
-    dir.create(path, recursive = TRUE, showWarnings = FALSE)
+    usethis::create_project(path = path, open = FALSE)
   }
 
   
   # create folder structure
-  dir.create(paste0(path, "/Data"))
-  dir.create(paste0(path, "/Data/01_Raw"))
-  dir.create(paste0(path, "/Data/02_Clean"))
+  dir.create(paste0(path, "/01_Data"))
+  dir.create(paste0(path, "/01_Data/01_Raw"))
+  dir.create(paste0(path, "/01_Data/02_Clean"))
   
   dir.create(paste0(path, "/R"))
   dir.create(paste0(path, "/R/src"))
-  file.create(paste0(path, "/R/src/fun_load_data.R"))
-  file.create(paste0(path, "/R/src/fun_helpers.R"))
-  file.create(paste0(path, "/R/src/fun_transform_data.R"))
+  file.create(paste0(path, "/R/load_data.R"))
+  file.create(paste0(path, "/R/helpers.R"))
+  
+  withr::with_dir(path, {
+    usethis::use_testthat()
+    usethis::use_test("load_data.R")
+    usethis::use_test("helpers.R")
+  })
 
-  dir.create(paste0(path, "/Analysis"))
-  file.create(paste0(path, "/Analysis/analysis.Rmd"))
-  file.create(paste0(path, "/Analysis/analysis.qmd"))
   
-  # usethis::use_test()
+  dir.create(paste0(path, "/02_Analysis"))
+  file.create(paste0(path, "/02_Analysis/analysis.Rmd"))
+  file.create(paste0(path, "/02_Analysis/analysis.qmd"))
   
-  dir.create(paste0(path, "/Documentation"))
-  dir.create(paste0(path, "/Documentation/01_Text"))
-  dir.create(paste0(path, "/Documentation/02_Final_figures"))
+  dir.create(paste0(path, "/03_Documentation"))
+  dir.create(paste0(path, "/03_Documentation/01_text"))
+  dir.create(paste0(path, "/03_Documentation/02_figures"))
   
-  dir.create(paste0(path, "/Outputs"))
-  dir.create(paste0(path, "/Outputs/01_Results"))
-  dir.create(paste0(path, "/Outputs/02_Figures"))
-  dir.create(paste0(path, "/Outputs/03_Tables"))
+  dir.create(paste0(path, "/04_Outputs"))
+  dir.create(paste0(path, "/04_Outputs/01_results"))
+  dir.create(paste0(path, "/04_Outputs/02_figures"))
+  dir.create(paste0(path, "/04_Outputs/03_tables"))
   
-  dir.create(paste0(path, "/Misc"))
-  dir.create(paste0(path, "/Misc/Public"))
-  dir.create(paste0(path, "/Misc/Priv"))
+  dir.create(paste0(path, "/05_Misc"))
+  dir.create(paste0(path, "/05_Misc/01_public"))
+  dir.create(paste0(path, "/05_Misc/02_priv"))
   
   # .gitignore for Azure DevOps
   gitignore_content <- c(
@@ -72,7 +80,7 @@ dau_proj_template <- function(
     ".Rhistory",
     ".RData",
     ".Ruserdata",
-    "Data"
+    "01_Data"
   )
   
   gitignore_content <- paste0(gitignore_content, collapse = "\n")
@@ -85,13 +93,15 @@ dau_proj_template <- function(
       ".Rhistory",
       ".RData",
       ".Ruserdata",
-      "Data",
-      "*/02_Results",
-      "*/03_Figures",
-      "*/04_Tables",
-      "Documentation/02_Final_figures",
-      "Outputs",
-      "*.html"
+      "01_Data",
+      "*/02_results",
+      "*/03_figures",
+      "*/04_tables",
+      "03_Documentation/02_figures",
+      "04_Outputs",      
+      "05_Misc/02_priv",
+      "*.html",
+      "*.xlsx"
     )
     
     gitignore_content <- paste0(gitignore_content, collapse = "\n")
@@ -105,38 +115,53 @@ dau_proj_template <- function(
     "This is the template for a standard data analysis.",
     "Please give an overview what you do in this project and how to navigate it.",
     "",
+    "## Introduction",
+    "TODO: Give a short introduction of your project. Let this section explain the objectives or the motivation behind this project.",
+    "",
+    "## Getting Started",
+    "TODO: Guide users through getting your code up and running on their own system. In this section you can talk about:",
+    "1.	Installation process",
+    "2.	Software dependencies",
+    "3.	Latest releases",
+    "4.	API references",
+    "",
+    "# Build and Test",
+    "TODO: Describe and show how to build your code and run the tests.",
+    "",
+    "# Contribute",
+    "TODO: Explain how other users and developers can contribute to make your code better.",
+    "",
     "## Git integration",
     "If you want to use git with your project (you should!), please do the following steps (replace `<name of your repository>` with the actual name):",
     "",
-    "1.  Go to your git repository provider (github/gitlab) and create a new repository",
+    "1.  Go to your git repository provider (GitHub/Azure DevOps) and create a new repository",
     "2.  DON'T check 'Add a README file'",
     "3.  Go to the Terminal within RStudio and type the following commands (for the URL, e.g. https://github.com):",
     "",
     "```bash",
     "git init",
     "git branch -M main",
-    "git remote add origin <URL of your github/gitlab instance>/<name of your repository>.git",
+    "git remote add origin <URL of your GitHub/Azure DevOps instance>/<name of your repository>.git",
     "```",
     "",
     "4.  Restart RStudio",
-    "5.  Select the files you want to commit in the git pane, click 'commit', in the pop-up write a commit message and click 'commit'",
+    "5.  Type in the R terminal `bash git add .` to add all files to the commit",
+    '5.  Type in the R terminal `bash git commit -m "Your commit message (initial commit)"` to commit those files with a message.',
     "6.  In the terminal, execute the following command:",
     "",
     "```bash",
     "git push -u origin main",
     "```",
     "",
-    "7.  For the following commits, you can use the 'push' button in the git pane",
+    "7.  For the following commits, repeat this process",
     "",
     "Please note that the following directories and files are not tracked by git by default (but you can change it in the .gitignore file):",
     "",
     "-   01_Data",
-    "-   all 02_Results folders",
-    "-   all 03_Figures folders",
-    "-   all 04_Tables folders",
-    "-   03_Manuscript/02_Final_figures",
-    "-   04_Presentation",
-    "-   all html files",
+    "",
+    "NOTE: For sharing content on GitHub you should have ticked the 'Create a .gitignore file for GitHub' checkbox when creating the projetc.",
+    "This will give create a strict .gitignore which is suitable for sharing code to the public.",
+    "Please also review to ensure no sensitive information is shared.",
     "",
     "For more information about the integration of git and RStudio, check out https://happygitwithr.com."
   )
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# The analysistemplates package
+# The dauRtemplate package
 
 The RStudio projects of your colleagues all have a different structure? It is
 difficult to navigate and understand others' projects? Or even your own project
 from a few months/years ago? Therefore, it is useful
 if you all agree on a standard folder structure for your analysis. Templates can
-help to enforce this. Currently, this package provides one standard data analysis
-template for RStudio projects geared towards scientific use cases. Whenever you
+help to enforce this. 
+
+This package provides one standard project folder structure template for 
+R projects geared towards DAU analysis use cases. Whenever you
 start a new project in RStudio, this package provides the standard folder structure
 directly in your code editor. Also, it can help you improving reproducibility by
 using git and `renv`.
@@ -24,54 +26,65 @@ To create a new project with the folder structure shown below, follow these step
 
 1.  Install the package
 2.  Restart RStudio
-3.  When creating a new RStudio project with the "New directory" option, choose "Standard analysis template"
+3.  When creating a new RStudio project with the "New directory" option, choose "DAU project template"
 
     ![RStudio Project Wizard showing the "Standard analysis template" option](man/figures/project_wizard_with_template.png)
 
-4.  During initialization you can select if a folder called `06_Analysis_for_publication` is included (check "Include Analysis for publication folder")
-5.  You can select if you want to generate a `.gitignore` file (check "Create a .gitignore file")
-6.  You can select if you want to use `renv` (check "Use renv with this project")
-7.  Once you've created the project, you will be provided with the instructions how to create a git repository for your project and connect to github/gitlab
+4.  During initialization you can select if you want to include a package structure 
+(check "Include folders and files required to build a R package (recommended).")
+5.  You can select if you want to generate a GitHub suitable `.gitignore` file (check "Create a .gitignore file for GitHub")
+7.  Once you've created the project, you will be provided with the instructions how to create a git repository for your project and connect to GitHub/Azure DevOps.
 
 ## Template overview
 
 A new project contains the following folder structure:
 
 ```
-|-- Data
+|-- 01_Data
 |   |-- 01_Raw
 |   `-- 02_Clean
-|   R
+|-- R
+|   |-- load_data.R
+|   |-- helpers.R
 |   |-- src
-|   |   |-- fun_load_data.R
-|   |   |-- fun_helpers.R
-|   |   |-- fun_transform_data.R
-|-- Analysis
+|-- tests
+|   |-- testthat.R
+|   |-- testthat
+|   |   |-- test-load_data.R
+|   |   |-- test-helpers.R
+|-- 02_Analysis
 |   |-- analysis.Rmd
 |   |-- analysis.qmd
-|-- Documentation
-|   |-- 01_Text
-|   `-- 02_Final_figures
-|-- Outputs
-|   |-- 01_Results              
-|   |-- 02_Figures              
-|   `-- 03_Tables    
-|-- Misc
-|   |-- Public              
-|   |-- Priv
+|-- 03_Documentation
+|   |-- 01_text
+|   `-- 02_figures
+|-- 04_Outputs
+|   |-- 01_results              
+|   |-- 02_figures              
+|   `-- 03_tables    
+|-- 05_Misc
+|   |-- 01_public              
+|   |-- 02_priv
 |-- README.md
-|-- .gitignore                  
+|-- proj_name.Rproj
+|-- .gitignore
+|-- .Rprofile
 |-- renv                        
 ```
 ## Planned features
 
-- better integration with git/github
+- Collaboration with DAU team to agree on folder and file structure.
+- Further testing and documentation of the package.
+- More support for a package folder structure.
+- Integration with QA (automated QA document).
+- Support for Shiny apps.
 
-## I want a different template
 
-You can easily create your own template. Fork this repo and create a new pair of
-`<my_template>.R` and `<my_template>.dcf` files. `<my_template>.dcf` defines
-how your template is named and which options are shown when you select the
-template in the project wizard. `<my_template>.R` defines what happens when the
-template is used. Have a look at `standard_analysis.R` and `standard_analysis.dcf`
-for inspiration or check out the [RStudio Extensions documentation](https://rstudio.github.io/rstudio-extensions/rstudio_project_templates.html).
+## Acknowledgements
+
+This repository is very much built on the repository 
+<a href="https://github.com/jonas-hag/analysistemplates" target="_blank">analysistemplates</a>
+by jonas-hag (Jonas Hagenberg).
+
+They should take all the credit for producing the core infrastructure of this
+package.
